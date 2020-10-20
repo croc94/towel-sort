fbffc8e3c49f3dfbc9175084c4cc240e8b895b2b
croc94 <croc94@mail.ru>
2020-10-20 14:24:21 +0300

    fix: clean code

--- src/index.js
+++ src/index.js
@@ -13,26 +13,19 @@ module.exports = function towelSort (matrix) {
 
     let result = [], matrix_length = matrix.length;
 
-
-
     for (let i = 1; i <= matrix_length; i++) {
 
         let count_arr = 0;
 
         if (i % 2 !== 0) {
-            // не четная функция
-
             for (let j = 1; j < i; j++) {
                 if (i === 1) {
                     continue;
                 }
                 count_arr += matrix[j - 1].length;
             }
-
             odd_arr_sort (matrix[i - 1], count_arr);
         } else {
-            //четная функция
-            
             for (let j = 0; j < i; j++) {
                 count_arr += matrix[j].length;
             }
@@ -41,22 +34,15 @@ module.exports = function towelSort (matrix) {
     }
 
     function odd_arr_sort (odd_arr, count) {
-
         for (let i = 0, odd_arr_length = odd_arr.length; i < odd_arr_length; i++) {
             result[i + count] = odd_arr[i];
         }
     }
 
     function even_arr_sort (even_arr, count) {
-
-
         for (let i = 0, even_arr_length = even_arr.length; i <  even_arr_length; i++) {
             result[count - i - 1] = even_arr[i];
         }
     };
-
-
-
-
     return result;
 }
